44ad6c83b8d3ee9bfd6326e4c4cbbe288afbd411
royweb <xytank@gmail.com>
2020-09-02 15:14:59 -0400

    Apply bootstrap and fortawesome to Vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap"      // for javascript
+import { library } from "@fortawesome/fontawesome-svg-core"
+
+import "bootstrap/dist/css/bootstrap.css"
+import "animate.css/animate.css"
+
+// Get icons from fortawesome
+import {
+  faShoppingCart,
+  faDollarSign
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart, faDollarSign)
 
 Vue.config.productionTip = false
 
