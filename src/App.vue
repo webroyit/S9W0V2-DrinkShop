9598904461d162c30c4087cfb5535edd328c4587
royweb <xytank@gmail.com>
2020-09-03 13:12:18 -0400

    Add some text for the header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <div id="app">
+  <div id="app" class="container mt-5">
     <h1>Drink Shop</h1>
+    <p class="animated fadeInRight">Here are the deals</p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default {
   name: 'App',
   components: {
-    
+    FontAwesomeIcon
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
