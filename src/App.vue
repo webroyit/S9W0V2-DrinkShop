dbcd6ebf0036672867e5eb1cc9bc0d514f258340
royweb <xytank@gmail.com>
2020-09-03 14:18:13 -0400

    Add a component to format price

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     <h1>Drink Shop</h1>
     <p class="animated fadeInRight">Here are the deals</p>
     <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <price value="9.99"></price>
   </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Price from './components/Price.vue'
 
 export default {
   name: 'App',
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon,
+    Price
   }
 }
 </script>
