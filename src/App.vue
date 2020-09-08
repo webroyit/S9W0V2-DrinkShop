7d27a0e93e098d8eea858b3026617e17877fcccf
royweb <xytank@gmail.com>
2020-09-08 13:23:14 -0400

    Use v-if to show certain drinks by price

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,19 @@
     
     <!-- v-for to loop through an array  -->
     <div class="row d-flex mb-3 align-items-center" v-bind:key="drink.id" v-for="drink in drinks">
-      <div class="col-sm-4">
-        <!--  v-bind bind a variable to an attribute -->
-        <img class="img-fluid d-block" v-bind:src="drink.image" v-bind:alt="name" />
-      </div>
-      <div class="col">
-        <h2 class="text-info">{{ drink.name }}</h2>
-        <p class="mb-0">{{ drink.description }}</p>
 
-        <!--  : works the same as v-bind -->
-        <price class="h5 float-right"  :value="drink.price"></price>
+      <div v-if="drink.price<=Number(max)">
+        <div class="col-sm-4">
+          <!--  v-bind bind a variable to an attribute -->
+          <img class="img-fluid d-block" v-bind:src="drink.image" v-bind:alt="name" />
+        </div>
+        <div class="col">
+          <h2 class="text-info">{{ drink.name }}</h2>
+          <p class="mb-0">{{ drink.description }}</p>
+
+          <!--  : works the same as v-bind -->
+          <price class="h5 float-right"  :value="drink.price"></price>
+        </div>
       </div>
     </div>
 
@@ -26,8 +29,6 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Price from './components/Price.vue'
 
-
-
 export default {
   name: 'App',
   components: {
@@ -36,10 +37,7 @@ export default {
   },
   data() {
     return {
-      name: "Soda",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi quam consequuntur culpa aliquam excepturi sunt magni minima eos tempora aperiam. Temporibus, unde? Assumenda dolores et enim tempora dolorem eligendi aliquam!",
-      image: "https://images.unsplash.com/photo-1504502350688-00f5d59bbdeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-      price: 1,
+      max: 4.5,
       drinks: [
         {
           id: 1,
