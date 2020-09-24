1205951c4f155b12cb657aa8cc2c26d55cd3330f
royweb <xytank@gmail.com>
2020-09-24 15:44:29 -0400

    Set up router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import "bootstrap"      // for javascript
 import { library } from "@fortawesome/fontawesome-svg-core"
@@ -14,8 +15,27 @@ import {
 
 library.add(faShoppingCart, faDollarSign)
 
+import Products from "./components/Products.vue";
+import Checkout from "./components/Checkout.vue";
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+// Set up the path to show a target page
+const router = new VueRouter({
+  routes: [
+    {
+      path: "*",
+      component: Products
+    },
+    {
+      path: "/checkout",
+      component: Checkout
+    }
+  ]
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
